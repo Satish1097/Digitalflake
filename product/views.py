4498026895d1a3bbfd3c9216fff7fb5cc69decdd
TotalRecall <satish@rankeval.ai>
2025-02-19 12:07:36 +0530

    log

--- product/views.py
+++ product/views.py
@@ -1,9 +1,12 @@
+import logging
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .models import Category,Subcategory,Product
 from .serializers import CategorySerializer,SubcategorySerializer,ProductSerializer
+
 
 # List and Create Category API
 class CategoryListCreateAPIView(APIView):
